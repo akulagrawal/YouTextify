8aacddcffb3667b7587ac31800f3a2100ff9eedc
Akul Agrawal <31360523+akulagrawal@users.noreply.github.com>
2018-12-12 09:11:00 +0530

    Using IBM Speech to Text API

--- transcribe.py
+++ transcribe.py
@@ -1,9 +1,9 @@
-import speech_recognition as sr
 from pydub import AudioSegment
 import math
 import sys
 import os
 import docx
+import json
 from download import urlToWav
 
 doc = docx.Document("youtube_list.docx")
@@ -16,43 +16,40 @@ for para in doc.paragraphs:
 	url = para.text[idx:]
 	videoName = "video" + str(idxVid)
 	idxVid += 1
-	urlToWav(url,videoName + ".webm")
+	urlToWav(url,videoName + ".webm", 'wav')
 
 	orig = AudioSegment.from_wav(videoName + ".wav")
 	n = len(orig)
-	d = 10000
+	d = 1000000
 	m = math.ceil(n/d)
 	fullText = ""
 
-	print("0.00%")
-
 	for i in range(0,m):
-		temp = orig[i*d:(i+1)*d]
-		temp.export("dummy.wav", format="wav")
+		print("Slot " + str(i+1) + " of " + str(m))
+		if i < m-1:
+			temp = orig[i*d:(i+1)*d]
+		else:
+			temp = orig[i*d:]
+		temp.export("dummy.flac", format="flac")
 
-		# transcribe audio file                                                         
-		AUDIO_FILE = "dummy.wav"
+		text = os.system("curl -X POST -u \"apikey:API_KEY\" \
+						  --header \"Content-Type: audio/flac\" \
+						  --data-binary @/home/akul/Desktop/Fathom/dummy.flac \
+						  \"URL/v1/recognize\" \
+						  > dummy.json")			# Enter your API_KEY and URL for Speech to Text IBM API
+		with open('dummy.json', 'r') as outfile:
+			data = json.load(outfile)
+			n = len(data['results'])
+			s = ""
+			for i in range(0,n):
+				s += data['results'][i]['alternatives'][0]['transcript'] + " "
+			fullText += s
 
-		# use the audio file as the audio source                                        
-		r = sr.Recognizer()
-		with sr.AudioFile(AUDIO_FILE) as source:
-			audio = r.record(source)  # read the entire audio file
+		#sys.stdout.write('\033[F')
 
-		try:
-			fullText += r.recognize_google(audio)	# requires internet connection
-													# Faster rates (but less accuracy) can be achieved by using offline:
-													# fullText += r.recognize_sphinx(audio)
-													# and increasing d (May also convert whole video directly using this)
-			fullText += " "
-		except:
-			fullText += ""
-
-		sys.stdout.write('\033[F')
-		print(str("{0:.2f}".format(((i+1)/m)*100)) + "%")
-
-	f = open(videoName + ".txt","w")
-	f.write(fullText)
-	f.close()
+		f = open(videoName + ".txt","w")
+		f.write(fullText)
+		f.close()
 
 if os.path.exists("dummy.wav"):
 	os.remove("dummy.wav")
